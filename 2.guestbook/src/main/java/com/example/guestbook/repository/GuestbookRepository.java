ae47745f2a43b9e01232decbc909631490b81623
samuel950523 <samuel95@naver.com>
2021-01-19 17:31:53 +0900

    delete unuse files

--- 2.guestbook/src/main/java/com/example/guestbook/repository/GuestbookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.guestbook.repository;
-
-import com.example.guestbook.entity.Guestbook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
-}
