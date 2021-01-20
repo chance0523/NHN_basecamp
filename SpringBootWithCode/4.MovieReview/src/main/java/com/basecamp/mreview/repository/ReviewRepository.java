aed8f66368d096bd184ef6927cf40034bee661ac
samuel950523 <samuel95@naver.com>
2021-01-20 14:32:59 +0900

    0120 특정 영화 조회 쿼리 (리뷰 조회)

--- SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/repository/ReviewRepository.java
+++ SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/repository/ReviewRepository.java
@@ -1,7 +1,15 @@
 package com.basecamp.mreview.repository;
 
+import com.basecamp.mreview.entity.Movie;
 import com.basecamp.mreview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
+
 }
