74d2e5519c767d47412254145f42f225c74e8205
samuel950523 <samuel95@naver.com>
2021-01-21 14:20:44 +0900

    0121 목록 처리 화면단

--- SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/controller/MovieController.java
+++ SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/controller/MovieController.java
@@ -1,10 +1,12 @@
 package com.basecamp.mreview.controller;
 
 import com.basecamp.mreview.dto.MovieDTO;
+import com.basecamp.mreview.dto.PageRequestDTO;
 import com.basecamp.mreview.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,14 @@ public class MovieController {
         redirectAttributes.addFlashAttribute("msg", mno);
 
         return "redirect:/movie/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("pageRequestDTO: " + pageRequestDTO);
+
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
+
     }
 }
--- SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/dto/MovieImageDTO.java
+++ SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/dto/MovieImageDTO.java
@@ -22,6 +22,15 @@ public class MovieImageDTO {
 
     public String getImageURL() {
         try {
+            return URLEncoder.encode(path + "/" + uuid + "_" + imgName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getThumbnailURL() {
+        try {
             return URLEncoder.encode(path + "/s_" + uuid + "_" + imgName, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
