3fbe05ef3cbbbc22b41c0d8e10736f52f3e23bbc
samuel950523 <samuel95@naver.com>
2021-01-21 11:01:53 +0900

    0121 영화 등록 처리 - register.html, 서버단

---
+++ SpringBootWithCode/4.MovieReview/src/main/java/com/basecamp/mreview/service/MovieServiceImpl.java
@@ -0,0 +1,42 @@
+package com.basecamp.mreview.service;
+
+
+import com.basecamp.mreview.dto.MovieDTO;
+import com.basecamp.mreview.entity.Movie;
+import com.basecamp.mreview.entity.MovieImage;
+import com.basecamp.mreview.repository.MovieImageRepository;
+import com.basecamp.mreview.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    private final MovieImageRepository imageRepository;
+
+    @Transactional
+    @Override
+    public Long register(MovieDTO movieDTO) {
+
+        Map<String, Object> entityMap = dtoToEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+
+        movieRepository.save(movie);
+
+        movieImageList.forEach(movieImage -> {
+            imageRepository.save(movieImage);
+        });
+
+        return movie.getMno();
+    }
+}
